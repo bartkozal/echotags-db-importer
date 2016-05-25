c8bdd5a24d8d8144a7193eb92aeed24756f9ee5e
Bartlomiej Kozal <bkzl@me.com>
2016-05-25 14:07:57 +0200

    Base view

---
+++ echotags-db-importer/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  echotags-db-importer
+//
+//  Created by bkzl on 25/05/16.
+//  Copyright © 2016 bkzl. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var pathLabel: UILabel!
+    @IBOutlet weak var categoriesCountLabel: UILabel!
+    @IBOutlet weak var pointsCountLabel: UILabel!
+    @IBOutlet weak var markersCountLabel: UILabel!
+}
